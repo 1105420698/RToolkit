be5d10a81e8cad295d2b933264bd38624a53d47e
Runkai Zhang <1105420698@qq.com>
2019-08-18 19:36:44 -0400

    Added documentation and some UNTESTED code.

--- Sources/RunkaiKit/RunkaiKit.swift
+++ Sources/RunkaiKit/RunkaiKit.swift
@@ -3,23 +3,48 @@
 import CloudKit
 #endif
 
-class Runkaikit {
-    func retrieveCloudKitRecord(withIdentifier id: String) -> CKRecord {
+public class RunkaiKit {
+    #if canImport(CloudKit)
+    /**
+     Retrieve the corresponding record with the given id. Throws an error when encounters one.
+    - parameters:
+        - id: The id that is going to be used.
+        - public: The location of the target database can be either public or private.
+      */
+    func retrieveCloudKitRecord(withIdentifier id: String, public: Bool) throws -> CKRecord  {
         var record: CKRecord?
+        var error: Error?
         let publicDatabase = CKContainer.default().publicCloudDatabase
         let recordID = CKRecord.ID(recordName: id)
-        publicDatabase.fetch(withRecordID: recordID, completionHandler: { recordRetrieved, error in
-            if let error = error {
-                print(error.localizedDescription)
+        
+        publicDatabase.fetch(withRecordID: recordID, completionHandler: { recordRetrieved, errorReceived in
+            if let errorReceived = errorReceived {
+                error = errorReceived
             } else {
                 record = recordRetrieved
             }
         })
         
-        return record!
+        if let error = error {
+            throw error
+        } else if let record = record {
+            return record
+        } else {
+            throw error!
+        }
     }
     
-    func retrieveHTML(fromURL url: String) -> String {
+    /**
+     Retrieve the raw HTML code from the URL provided.
+     - parameters:
+        - url: A string containing the URL of your target webpage.
+     
+     - Returns:
+        - The HTML in its `String` form if the process succeeds.
+        - An error if it the process fails.
+        - An error if the input is not a valid URL.
+     */
+    func retrieveHTML(from url: String) -> String {
         guard let myURL = URL(string: url) else {
             print("Error: \(url) doesn't seem to be a valid URL")
             return "Error: \(url) doesn't seem to be a valid URL"
@@ -34,7 +59,6 @@ class Runkaikit {
     }
     
     func isIcloudAvailable() -> Bool {
-        
         var result: Bool?
         
         CKContainer.default().accountStatus(completionHandler: { accountStatus, error in
@@ -47,6 +71,7 @@ class Runkaikit {
         
         return result!
     }
+    #endif
     
     func isFirstTimeLaunch() -> Bool {
         if (UserDefaults.standard.bool(forKey: "HasLaunchedOnce")) {

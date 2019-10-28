cf4a9dad80256d40ad2c7dd368d60eeff87a86d1
Runkai Zhang <1105420698@qq.com>
2019-10-27 22:31:32 -0400

    Added the power function and did structural adjustments to RMath.

--- Sources/RunkaiKit/RMath.swift
+++ Sources/RunkaiKit/RMath.swift
@@ -7,7 +7,43 @@
 
 import Foundation
 
-/// /// The main structure of RunkaiKit where all the math operations that is supported can be done.
-public struct RMath {
+/// A structure that contains all of the RMath functions.
+public struct RMFunction {
     
+    /// A power function, where both k and n are constants, creates a parabola.
+    public class power {
+        /// A constant.
+        public var k: Double
+        /// A constant, the exponent of `x`.
+        public var n: Double
+        /// The lower bound of `x` in the calculation.
+        public var xLowerBound: Int
+        /// The upper bound of `x` in the calculation.
+        public var xUpperBound: Int
+        
+        /// Initialize a power function with two constants: `k` and `n`.
+        init(k: Double, n: Double, xLowerBound: Int, xUpperBound: Int) {
+            self.k = k
+            self.n = n
+            self.xLowerBound = xLowerBound
+            self.xUpperBound = xUpperBound
+        }
+        
+        /// Calculate the power function with the values it has.
+        public func calculate(completionHandler: ([Double]) -> Void) {
+            var result = [Double]()
+            
+            for x in xLowerBound...xUpperBound {
+                if x != xUpperBound {
+                    let answer = k * pow(Double(x), n)
+                    result.append(answer)
+                } else {
+                    let answer = k * pow(Double(x), n)
+                    result.append(answer)
+                }
+            }
+            
+            completionHandler(result)
+        }
+    }
 }
--- Tests/RunkaiKitTests/RunkaiKitTests.swift
+++ Tests/RunkaiKitTests/RunkaiKitTests.swift
@@ -2,13 +2,15 @@ import XCTest
 @testable import RunkaiKit
 
 final class RunkaiKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    func testMath() {
+        let power = RMFunction.power(k: Double(1), n: Double(2), xLowerBound: -5, xUpperBound: 5)
+        power.calculate { (result) in
+            XCTAssertEqual(result.first, 25, "This should be exactly the same!")
+            XCTAssertEqual(result.last, 25, "This should be exactly the same!")
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testMath", testMath),
     ]
 }
